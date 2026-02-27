6d09d11326132c27624760ca7bc39e4d12196c74
RK332 <134835563+RK332@users.noreply.github.com>
2026-02-27 14:56:16 +0800

    Initial upload of vue-shop project

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // 部署应用包时的基本 URL，这里放在 /shop 子路径下
+  publicPath: '/shop/',
+  // 打包输出目录，保持默认的 dist 即可
+  outputDir: 'dist',
+  // 生产环境关闭 source map，减小体积
+  productionSourceMap: false
+};
+
